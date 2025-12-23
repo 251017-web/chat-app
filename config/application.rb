c87fe93a91a0176cc681c584cb9d7c2fcfdccd8d
251017-web <zlba251017@gmail.com>
2025-12-23 21:21:15 +0900

    空のメッセージが送信できないバリデーションの設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.i18n.available_locales = [:ja, :en]
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
 
